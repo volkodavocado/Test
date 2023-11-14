cc9e051ad758b5e0621d6b77e1f6254cdcd09851
volkodavocado <145379339+volkodavocado@users.noreply.github.com>
2023-11-14 22:14:00 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,126 +1,83 @@
-import java.util.Scanner;
+import java.util.*; // импортируем сканнер и массивы
 
-public class Main {
+public class Main { // создаем класс main
 
-    public static void main(String[] args) {
-        System.out.println("Введите выражение: ");
-        Scanner s = new Scanner(System.in);
+    public static void main(String[] args) { // создаем главный метод
+        Scanner s = new Scanner(System.in);// принимает строку
         String input = s.nextLine();
-        calc(input);
+        System.out.println(calc(input));
+        // и возвращает результат
     }
-    public static String calc(String input) {
-
-        String operStr = " ";
-        char oper = 0;
-        char[] symbol = new char[10];
-        for (int i=1; i < input.length(); i++) {
-            symbol[i] = input.charAt(i);
-            if ( symbol[i] == '+'){
-                oper = '+'; operStr = "\\+";
-            }
-            if ( symbol[i] == '-'){
-                oper = '-'; operStr = "-";
-            }
-            if ( symbol[i] == '*'){
-                oper = '*'; operStr = "\\*";
-            }
-            if ( symbol[i] == '/'){
-                oper = '/'; operStr = "/";
-            }
-        }
-
-        int num1 = 0;
-        int num2 = 0;
-        int result = 0;
-        int resultArab = 0;
-        String[] numbers = input.split(" ");
-        if (numbers.length > 4) {
+    public static String calc(String input) { // метод работает с числами
+        String[] array = input.split(" "); // создает массив
+        if (array.length > 3) { // исключение на кол-во символов
             throw new IllegalArgumentException();
         }
-        try{
-            num1 = romanNumeral(numbers[0]);
-            num2 = romanNumeral(numbers[2]);
-        } catch (ArrayIndexOutOfBoundsException e) {
-            throw new IllegalArgumentException();
-        }
-        if (num1 == 0 | num2 == 0) {
-            result = 0;
+        int num1, num2, result, resultArab;
+        String resultAr = null, myResult = null, resultRom = null; // инициализация переменных
+        num1 = romanNumeral(array[0]); // проверка на римские
+        num2 = romanNumeral(array[2]);
+        if (num1 == 0 | num2 == 0) { // если не римские
             try {
-                num1 = Integer.parseInt(numbers[0]);
-                num2 = Integer.parseInt(numbers[2]);
+                num1 = Integer.parseInt(array[0]); // перевод в арабские
+                num2 = Integer.parseInt(array[2]);
                 if (num1 > 10 | num2 > 10 | num1 < 1 | num2 < 1) {
-                    throw new IllegalArgumentException();
+                    throw new IllegalArgumentException(); // проверка введ. арабских
                 }
-                resultArab = calculate(num1, num2, oper);
-                System.out.println(String.valueOf(resultArab));
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException();
-            } catch (ArithmeticException e) {
-                throw new IllegalArgumentException();
+                resultArab = calculate(num1, num2, array[1]); // счет
+                resultAr = String.valueOf(resultArab);
+                //System.out.println(String.valueOf(resultArab)); // вывод
+            } catch (NumberFormatException | ArithmeticException e) {
+                throw new IllegalArgumentException(); // исключение на формат ввода
             }
-        } else {
+        } else { // если римские
             try {
-                result = calculate(num1, num2, oper);
-                if (result == 0) {
-                    throw new IllegalArgumentException();
+                result = calculate(num1, num2, array[1]); // счет
+                if (result <= 0) {
+                    throw new IllegalArgumentException(); // исключение на отриц рез
                 }
-                String resultRom = romanSolution(result);
-                System.out.println(resultRom);
+                resultRom = romanSolution(result); // перевод обратно в римское
+                //System.out.println(resultRom); // вывод строки
             } catch (ArrayIndexOutOfBoundsException e) {
                 throw new IllegalArgumentException();
             }
         }
-        return String.valueOf(resultArab);
-    }
-
-    public static int calculate(int x1, int x2, char op) {
-        int result = 0;
-        switch (op) {
-            case '+':
-                result = x1 + x2;
-                break;
-            case '-':
-                result = x1 - x2;
-                break;
-            case '*':
-                result = x1 * x2;
-                break;
-            case '/':
-                result = x1 / x2;
-                break;
-            default:
-                break;
+        if (resultAr != null) {
+            myResult = resultAr;
         }
-        return result;
-    }
-
-    public static int romanNumeral(String roman) {
-        if (roman.equals("I")) {
-            return 1;
-        } else if (roman.equals("II")) {
-            return 2;
-        } else if (roman.equals("III")) {
-            return 3;
-        } else if (roman.equals("IV")) {
-            return 4;
-        } else if (roman.equals("V")) {
-            return 5;
-        } else if (roman.equals("VI")) {
-            return 6;
-        } else if (roman.equals("VII")) {
-            return 7;
-        } else if (roman.equals("VIII")) {
-            return 8;
-        } else if (roman.equals("IX")) {
-            return 9;
-        } else if (roman.equals("X")) {
-            return 10;
-        } else {
-            return 0;
+        if (resultRom != null) {
+            myResult = resultRom;
         }
+        return myResult;
     }
 
-    public static String romanSolution(int arabNumeral) {
+    static int calculate(int x1, int x2, String op) {
+        return switch (op) {
+            case "+" -> x1 + x2;
+            case "-" -> x1 - x2;
+            case "*" -> x1 * x2;
+            case "/" -> x1 / x2;
+            default -> throw new IllegalArgumentException();
+        };
+    }
+
+    static int romanNumeral(String roman) {
+        return switch (roman) {
+            case "I" -> 1;
+            case "II" -> 2;
+            case "III" -> 3;
+            case "IV" -> 4;
+            case "V" -> 5;
+            case "VI" -> 6;
+            case "VII" -> 7;
+            case "VIII" -> 8;
+            case "IX" -> 9;
+            case "X" -> 10;
+            default -> 0;
+        };
+    }
+
+    static String romanSolution(int arabNumeral) {
         String[] romanAll = {"O", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X",
                 "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
                 "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX",
@@ -133,6 +90,5 @@ public class Main {
                 "LXXXIX", "XC", "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"
         };
         return romanAll[arabNumeral];
-
     }
 }
